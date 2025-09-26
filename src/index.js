81ed360d01699f281af1ff88472cc98b66066531
daanniill <daniil.malakhov12@gmail.com>
2025-09-26 16:20:48 -0700

    created class prototypes for tasks and projects instances

--- src/index.js
+++ src/index.js
@@ -1,2 +1,75 @@
 import "./styles.css"
 import "./reminders.css"
+
+//storage
+let tasks = []
+let projects = []
+
+//class prototypes
+class Task {
+    constructor(title, description, priority, date, category){
+        this._title = title;
+        this._description = description;
+        this._priority = priority;
+        this._date = date;
+        this._category = category;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get description(){
+        return this._description;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    get category() {
+        return this._category;
+    }
+}
+
+class Project {
+    constructor(title){
+        this._title = title;
+        this._tasks = [];
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get tasks() {
+        return this._tasks;
+    }
+
+    addTask(task){
+        if (task instanceof Task) {
+            this._tasks.push(task);
+        }
+        else {
+            throw new Error("Only Task instances can be added.");
+        }
+    }
+
+    removeTask(taskTitle) {
+        this._tasks = this._tasks.filter(task => task.title !== taskTitle);
+    }
+
+    getTask(taskTitle) {
+        return this._tasks.find(task => task.title === taskTitle);
+    }
+
+    getAllTasks() {
+        return this._tasks;
+    }
+}
+
+//pre-populate projects
